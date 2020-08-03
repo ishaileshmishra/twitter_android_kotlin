4c0cd69830ea91dc9ac80f277f98be655bab8484
mshaileshr@gmail.com <Yes@383840>
2020-08-03 23:48:37 +0530

    Main page and fragments added

--- app/src/main/java/com/initone/twitter/view/ui/MainActivity.java
+++ app/src/main/java/com/initone/twitter/view/ui/MainActivity.java
@@ -1,5 +1,4 @@
 package com.initone.twitter.view.ui;
-
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.initone.twitter.R;
@@ -18,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search,
-                R.id.navigation_notifications, R.id.navigation_message)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(
+                        R.id.navigation_home, R.id.navigation_search,
+                        R.id.navigation_notifications, R.id.navigation_message)
+                        .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
